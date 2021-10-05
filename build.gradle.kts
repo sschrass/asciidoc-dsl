9e17efff8a14e2880e70d8a56e997941a0866366
Stefan Schrass <stefan.schrass@gmail.com>
2021-10-05 13:07:29 +0200

    java 16

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.5.31"
     id("io.gitlab.arturbosch.detekt") version "1.18.1"
     id("com.github.ben-manes.versions") version "0.39.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    id("com.asarkar.gradle.build-time-tracker") version "3.0.1"
 }
 
 group = "de.sschrass"
@@ -14,4 +17,15 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_16.toString()
+    }
 }
