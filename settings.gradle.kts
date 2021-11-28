d2dd9cbe796d10a69801ddf0e0953f28249cb4c2
Stefan Schrass <stefan.schrass@gmail.com>
2021-11-28 18:05:04 +0100

    ci experiment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+plugins {
+    id("com.gradle.enterprise") version ("3.7.2")
+}
+
 rootProject.name = "AsciidocDsl"
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
